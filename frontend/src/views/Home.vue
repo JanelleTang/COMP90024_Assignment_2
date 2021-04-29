48a67a6507f282262b2942eeefa0c552915712f3
Janelle Tang <41579252+JanelleTang@users.noreply.github.com>
2021-04-29 17:44:16 +1000

    joined front and back with axios

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+
+    <div>     <Navbar></Navbar>
+    hello </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+    import Navbar from '../components/Navbar'
+    export default{
+        name:'Home',
+        components:{
+            Navbar
+        },
+    }
 </script>
