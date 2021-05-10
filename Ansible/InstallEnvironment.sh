83fbe4f008c924872870a4770c312134772e7dc6
Shuang Qiu <qsq@student.unimelb.edu.au>
2021-05-10 21:49:09 +1000

    finish installing environment

--- Ansible/InstallEnvironment.sh
+++ Ansible/InstallEnvironment.sh
@@ -1,3 +1,3 @@
 # COMP90024 Project - Team 37
 
-. ./openrc.sh; ansible-playbook --ask-become-pass InstallEnvironment.yaml
+. ./openrc.sh; ansible-playbook --ask-become-pass InstallEnvironment.yaml -i inventory/inventory.ini --private-key ~/.ssh/dev-testing.pem
