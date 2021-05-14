83cc462e2a2a73d51830e5b4a4ad525b231abb03
Shuang Qiu <qsq@student.unimelb.edu.au>
2021-05-15 00:35:45 +1000

    add proxy config for docker

--- Ansible/DeployApplication.sh
+++ Ansible/DeployApplication.sh
@@ -1,4 +1,4 @@
 
 
 export ANSIBLE_HOST_KEY_CHECKING=False
-. ./openrc.sh; ansible-playbook --ask-become-pass DeployApplication.yaml -i inventory/inventory.ini
+. ./openrc.sh; ansible-playbook --ask-become-pass DeployApplication.yaml -i inventory/inventory.ini --private-key ~/.ssh/dev-testing.pem
