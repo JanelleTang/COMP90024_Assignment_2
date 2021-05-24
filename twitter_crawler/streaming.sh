ce0a013e8251326f54b9655db936c2966e51a278
DataScienceDec <80931200+DataScienceDec@users.noreply.github.com>
2021-05-24 18:53:31 +1000

    Update streaming.sh

--- twitter_crawler/streaming.sh
+++ twitter_crawler/streaming.sh
@@ -1,3 +1,19 @@
+# ============= COMP90024 - Assignment 2 ============= #
+#                               
+# The University of Melbourne           
+# Team 37
+#
+# ** Authors: **
+# 
+# JJ Burke              1048105
+# Janelle Tang          694209
+# Shuang Qiu            980433
+# Declan Baird-Watson   640975
+# Avinash Rao           1024577 
+# 
+# Location: Melbourne
+# ==================================================== 
+
 #!/bin/bash
 nohup python3 -u ./twitter_streaming2.py > output2.log &
 sleep 10s
